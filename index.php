bb9a9cf9afe97a53632a09281a97d0a2d7cd075e
DejMar <marjanovic.dejan@gmail.com>
2026-06-21 18:21:09 +0200

    adding new documentation

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ require_once __DIR__ . '/includes/header.php';
     <h1>Dobrodošli u naš hotel</h1>
     <p>Pregledajte dostupne sobe i rezervišite svoj boravak online.</p>
     <a href="dokumentacija.html" class="btn btn-outline hero-docs-btn">Dokumentacija projekta</a>
+    <a href="php-fajlovi.html" class="btn btn-outline hero-docs-btn">PHP fajlovi</a>
 </section>
 
 <section class="filters">
